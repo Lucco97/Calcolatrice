1432287fc5afd72ce0c759321932f2352399d337
Andrea <spada96@hotmail.it>
2015-02-21 12:49:13 +0100

    Update AppDelegate.swift

--- Calcolatrice/AppDelegate.swift
+++ Calcolatrice/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Calcolatrice
 //
-//  Created by Fabio Luccon on 12/01/15.
+//  Created by Fabio Luccon and Andrea Spada on 12/01/15.
 //  Copyright (c) 2015 Fabio Luccon. All rights reserved.
 //
 
